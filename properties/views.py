b95468c700abb1cb0b1342b75d8902aa69f5b065
Wege0921 <wegemuluneh@gmail.com>
2025-08-31 09:52:49 -0400

    localhost checked

--- properties/views.py
+++ properties/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render
-from django.views.decorators.cache import cache_page
 from django.http import JsonResponse
+from django.views.decorators.cache import cache_page
 from .models import Property
-
 
 @cache_page(60 * 15)  # cache for 15 minutes
 def property_list(request):
     properties = Property.objects.all().values(
         'id', 'title', 'description', 'price', 'location', 'created_at'
     )
-    return JsonResponse(list(properties), safe=False)
+
+    # return JSON response with "data" key
+    return JsonResponse({"data": list(properties)})
